c048fc07c0c211355df2f3c425fd6c1331f7b83b
sergiotto <sergiotto@outlook.com>
2022-12-28 18:20:10 +0100

    formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Modal,
   SafeAreaView,
@@ -6,15 +6,62 @@ import {
   Text,
   TextInput,
   View,
+  ScrollView,
 } from 'react-native';
 
 const Formulario = ({modalVisible}) => {
+  const [paciente, setPaciente] = useState('')
+  
   return (
     <Modal animationType="slide" visible={modalVisible}>
       <SafeAreaView style={styles.contenido}>
-        <Text style={styles.titulo}>
-          Nueva <Text style={styles.tituloBold}>Cita</Text>
-        </Text>
+        <ScrollView>
+          <Text style={styles.titulo}>
+            Nueva <Text style={styles.tituloBold}>Cita</Text>
+          </Text>
+          <View style={styles.campo}>
+            <Text style={styles.label}>Nombre Paciente</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nombre paciente"
+              placeholderTextColor={'#666'}
+            />
+          </View>
+          <View style={styles.campo}>
+            <Text style={styles.label}>Nombre Propietario</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nombre propietario"
+              placeholderTextColor={'#666'}
+            />
+          </View>
+          <View style={styles.campo}>
+            <Text style={styles.label}>Email Propietario</Text>
+            <TextInput
+              keyboardType="email-address"
+              style={styles.input}
+              placeholder="email propietario"
+              placeholderTextColor={'#666'}
+            />
+          </View>
+          <View style={styles.campo}>
+            <Text style={styles.label}>Teléfono Propietario</Text>
+            <TextInput
+              keyboardType="number-pad"
+              style={styles.input}
+              placeholder="Teléfono propietario"
+              placeholderTextColor={'#666'}
+            />
+          </View>
+          <View style={styles.campo}>
+            <Text style={styles.label}>Sintomas</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Síntomas"
+              placeholderTextColor={'#666'}
+            />
+          </View>
+        </ScrollView>
       </SafeAreaView>
     </Modal>
   );
@@ -33,5 +80,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   tituloBold: {fontWeight: '900'},
+  campo: {
+    marginTop: 10,
+    marginHorizontal: 30,
+    marginBottom: 5,
+  },
+  label: {
+    color: '#fff',
+    marginBottom: 10,
+
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  input: {
+    padding: 15,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    marginBottom: 10,
+  },
 });
 export default Formulario;
