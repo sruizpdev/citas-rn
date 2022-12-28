150419cc0e16794dd00c51bcc3daf4eee705f806
sergiotto <sergiotto@outlook.com>
2022-12-28 12:09:35 +0100

    button Nueva Cita added

--- App.js
+++ App.js
@@ -1,115 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Text, SafeAreaView, StyleSheet, Pressable} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+const App = () => {
+  const nuevaCitaHandler = () => {
+    console.log('presionaste...');
   };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Readsss the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>
+        Administrador de Citas{' '}
+        <Text style={styles.tituloBold}>Veterinaria</Text>
+      </Text>
+      <Pressable style={styles.btnNuevaCita} onPress={nuevaCitaHandler}>
+        <Text style={styles.btnTextNuevaCita}>Nueva Cita</Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
+  container: {backgroundColor: '#f3f4f6', flex: 1},
+  titulo: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
     fontWeight: '600',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  tituloBold: {
+    fontWeight: '900',
+    color: '#6d28d9',
   },
-  highlight: {
-    fontWeight: '700',
+  btnNuevaCita: {
+    backgroundColor: '#6d28d9',
+    padding: 20,
+    marginTop: 20,
+    marginHorizontal: 20,
+    borderRadius: 10,
+  },
+  btnTextNuevaCita: {
+    textAlign: 'center',
+    fontSize: 17,
+    textTransform: 'uppercase',
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
 
